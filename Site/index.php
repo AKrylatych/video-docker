4604688fb9b994ab778cd0e18c4f5bac36d988bd
Hellerlight <hellerlight@gmail.com>
2023-06-07 20:52:23 +0300

    starting work on api, finished the setup

--- Site/index.php
+++ Site/index.php
@@ -6,11 +6,16 @@
 </head>
 <body>
 <form action="http://api.video-docker.online/apitest/statuscheck.php" method="get">
-    test <br>
+    login <br>
     <label>
         <input name="text" type="text">
     </label>
     <input type="submit" value="send text" name="submit">
 </form>
+<a href="view.php">View curl</a><br>
+
+<a href="login.html">Login page</a><br>
+<a href="register.html">Register page</a><br>
+<a href="http://api.video-docker.online/apitest/phpinfo.php">Phpinfo</a>
 </body>
 </html>
--- Site/testing.php
+++
@@ -1,6 +0,0 @@
-<?php
-header('Content-Type: application/json');
-$output = array(
-    "text" => "You're supposed to see this"
-);
-print_r(json_encode($output));
